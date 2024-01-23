ac2888b1f687fe216b2bfb51749b2458be8e539a
Jaap223 <6003958@mborijnland.nl>
2024-01-23 12:51:02 +0100

    a

--- Product.php
+++ Product.php
@@ -3,8 +3,6 @@ require_once 'vendor/autoload.php';
 require_once 'head/Head.php';
 require_once 'int/db.php';
 
-
-
 class Product extends Database
 {
 
@@ -28,24 +26,32 @@ class Product extends Database
 }
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(isset($_POST['addProduct'])) {
+
+
+    }
+
+}
 ?>
 
 <section class="formR">
     <form method="post">
-        <label for="product_name">Naam</label>
+        <label for="product_name">Product_name</label>
         <input type="text" name="product_name" id="product_name">
 
-        <label for="category_id">Wachtwoord</label>
+        <label for="category_id">category_id</label>
         <input type="text" name="category_id" id="category_id">
 
-        <label for="price">Adres</label>
+        <label for="price">price</label>
         <input type="text" name="price" id="price">
 
-        <label for="stock_quantity">Telefoonnummer</label>
-        <input type="text" name="tel_nr" id="tel_nr">
+        <label for="stock_quantity">stock_quantity</label>
+        <input type="text" name="stock_quantity" id="quantity">
 
-        <input type="submit" name="Products" value="Products">
+        <input type="submit" name="addProduct" value="addProduct">
     </form>
 
-    <a href="Login.php">Inloggen</a>
+    <a href="Categoriess.php">Categoriess</a>
 </section>f
